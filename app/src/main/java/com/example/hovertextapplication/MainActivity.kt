1e60401f2132e94998a2f9901d8698712a175003
Odu Anthony <teerox44@gmail.com>
2020-05-07 22:33:37 +0100

    Test Integration

--- app/src/main/java/com/example/hovertextapplication/MainActivity.kt
+++ app/src/main/java/com/example/hovertextapplication/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.hovertextapplication
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.hover.sdk.actions.HoverAction
 import com.hover.sdk.api.Hover
@@ -16,12 +19,12 @@ import java.util.*
 
 class MainActivity : AppCompatActivity(), Hover.DownloadListener {
 
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Hover.initialize(getApplicationContext(), this)
-
+        Hover.initialize(applicationContext, this)
 
         permissions_button.setOnClickListener {
             val i = Intent(applicationContext, PermissionActivity::class.java)
@@ -33,11 +36,7 @@ class MainActivity : AppCompatActivity(), Hover.DownloadListener {
         button.isEnabled = true
         button.setOnClickListener {
                 val i = HoverParameters.Builder(this@MainActivity)
-                    .request("83a57bf3")
-                    .extra("account","00*****623")
-                    .extra("amount","100")
-                    .extra("phoneNumber","09025748018")
-                    // Uncomment and add your variables if any
+                    .request("ddd43c9c")
                     .buildIntent()
                 startActivityForResult(i, 0)
         }
@@ -54,6 +53,20 @@ class MainActivity : AppCompatActivity(), Hover.DownloadListener {
         Log.e("Error Message",p0.toString())
     }
 
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK) {
+            val sessionTextArr = data?.getStringArrayExtra("session_messages")
+            val check = sessionTextArr?.get(sessionTextArr.size - 1)
+            val myTex = findViewById<View>(R.id.text) as TextView
+            myTex.text = check
+            val uuid = data?.getStringExtra("uuid")
+            Log.e(TAG, "Return array: $check")
+            Log.e(TAG, "Return uuid: $uuid")
+        } else if (requestCode == 0 && resultCode == Activity.RESULT_CANCELED) {
+            Toast.makeText(this, "Error: " + data?.getStringExtra("error"), Toast.LENGTH_LONG).show()
+        }
+    }
 
 }
 
